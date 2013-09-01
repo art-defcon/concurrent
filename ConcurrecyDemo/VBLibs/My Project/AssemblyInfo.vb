8ee62fee89d564a2591d282c5381be3dd1134220
John Petroff <git@petroff.org>
2013-09-01 20:55:59 +0200

    Started sln

---
+++ ConcurrecyDemo/VBLibs/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VBLibs")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("VBLibs")> 
+<Assembly: AssemblyCopyright("Copyright ©  2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("80cc3898-7b14-42a8-8aa7-b687b29a8e0e")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ ConcurrecyDemo/VBLibs/SingletonPattern/Singelton.vb
@@ -0,0 +1,13 @@
+﻿Public Class Singleton
+    Private Sub New()
+
+    End Sub
+
+    Public Shared ReadOnly Property GetInstance As Singleton
+        Get
+            Static Instance As Singleton = New Singleton
+            Return Instance
+        End Get
+    End Property
+
+End Class
